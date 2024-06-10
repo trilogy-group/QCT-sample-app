3c73a74d2aeecc6fed960b7af375dd71337ed8c2
Vinayak Sachdeva <vinayak.sachdeva@codenation.co.in>
2024-06-10 18:22:18 +0000

    2024-06-10_18-20-24

--- src/test/java/com/bhoruka/bloodbank/web/CampControllerTest.java
+++ src/test/java/com/bhoruka/bloodbank/web/CampControllerTest.java
@@ -12,10 +12,10 @@ import com.bhoruka.bloodbank.service.CampService;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.Assertions;
-import org.mockito.Mock;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
